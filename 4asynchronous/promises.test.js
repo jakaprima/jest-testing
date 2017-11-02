e083456ae7f73cd3b7bb2970345e39f33655fdc8
jakaprima <jakaprima123@gmail.com>
2017-11-02 10:53:56 +0700

    resolves

--- 4asynchronous/promises.test.js
+++ 4asynchronous/promises.test.js
@@ -1,6 +1,7 @@
-// promises
-test('test promises', () => {
-	let fetchData = new Promise((resolve, reject) => {
+// promises : object represent resolve atau reject operasi async & nilai yang dihasilkan
+
+test('test promises done', () => {
+	let fetchData = new Promise((resolve, reject) => { //executor func
 		setTimeout(function(){
 			resolve('berhasil'); // resolve untuk handle jika asynch berhasil
 		}, 250);
@@ -8,7 +9,23 @@ test('test promises', () => {
 
 	expect.assertions(1);
 
-	return fetchData.then((data) => {
+	return fetchData.then((data) => { // harus pake return
 		expect(data).toBe('berhasil');
 	});
+
+	return fetchData.catch((e) => {
+		expect(e).toMatch('error')
+	});
+});
+
+test('test promises reject', () => {
+	let fetchData = new Promise((resolve, reject) => {
+		setTimeout(() => {
+			reject('erroraja');
+		}, 250)
+	});
+
+	return fetchData.catch((e) => {
+		expect(e).toMatch('error');
+	})
 });
---
+++ 4asynchronous/resolves.test.js
@@ -0,0 +1,20 @@
+// .resolves mencocokan expect statement
+// jest akan tunggu promise itu sampai resolve
+
+
+test('check promise if promise to resolve or reject', () => {
+	let p1 = Promise.resolve(4);
+	let p2 = 1003;
+	let p3 = new Promise((resolve, reject) => {
+		setTimeout(resolve, 100, 'test')
+	})
+
+	
+	Promise.all([p1, p2, p3]).then((values) => {
+		expect.assertions(1);
+		return expect(values).resolves.toBe([4, 1003, 'test'])
+	});
+
+
+});
+
